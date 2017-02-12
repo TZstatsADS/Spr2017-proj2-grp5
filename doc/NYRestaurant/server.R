13f07fae8369bcd7cf28849e152ae7ba715ac4ec
jz2773 <jz2773@columbia.edu>
2017-02-12 17:32:59 -0500

    msg

--- doc/NYRestaurant/server.R
+++ doc/NYRestaurant/server.R
@@ -16,8 +16,8 @@ shinyServer(function(input, output) {
     output$barPlot <- renderPlot({
         
         selected <- orig_1617[(orig_1617$CUISINE.DESCRIPTION==input$type),]
-        df <- as.data.frame(count(selected$recode))
-        ggplot(data=df, aes(reorder(x,freq),freq)) +geom_bar(stat="identity")+
+        df <- as.data.frame(table(selected$recode))
+        ggplot(data=df, aes(reorder(Var1,Freq),Freq)) +geom_bar(stat="identity")+
           coord_flip()
         
         
@@ -26,10 +26,10 @@ shinyServer(function(input, output) {
     output$barPlot1 <- renderPlot({
       selected <- orig_1617[(orig_1617$CUISINE.DESCRIPTION==input$type),]
       selected_vio <- selected[(selected$recode==input$vio_type),]
-      df1<- as.data.frame(table(seleccted_vio$VIOLATION.DESCRIPTION))
+      df1<- as.data.frame(table(selected_vio$VIOLATION.DESCRIPTION))
       df1 <-df1[df1$Freq!=0,]
       x <- c(1:nrow(df1))
-      plot_ly(df1, x = ~reorder(x,Freq), y = ~Freq, type = 'bar', text = as.vector(df1$Var1),
+      plot_ly(df1, x = ~reorder(x,Freq), y = ~Freq, type = 'bar', hoverinfo="text",text = as.vector(df1$Var1),
                    marker = list(color = 'rgb(158,202,225)',
                                  line = list(color = 'rgb(8,48,107)',
                                              width = 1))) %>%
